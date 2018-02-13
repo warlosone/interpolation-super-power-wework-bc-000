d2fffed8ff3e1440d0dbfb63951d93ae51d002ae
Mars Santos <mars@meetup.com>
2018-02-13 02:48:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(x)
+  puts "R: #{x}[]"
+  puts "R: #{x}[]"
+  puts "R: #{x}[]"
+  puts "R: #{x}[]"
+  puts "R: #{x}[]"
+  puts "R: #{x}[]"
+  puts "R: #{x}[]"
+end
