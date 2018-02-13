7c15a34db6e36587d9f0767253f4b0a4632e02c2
Mars Santos <mars@meetup.com>
2018-02-13 02:54:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 # Write your #display_rainbow method here
 def display_rainbow(x)
-  puts "R: #{x}[]"
-  puts "O: #{x}[]"
-  puts "Y: #{x}[]"
-  puts "G: #{x}[]"
-  puts "R: #{x}[]"
-  puts "B: #{x}[]"
+  puts "R: [#{x}]"
+  puts "O: [#{x}]"
+  puts "Y: [#{x}]"
+  puts "G: [#{x}]"
+  puts "R: [#{x}]"
+  puts "B: [#{x}]]"
   puts "I: #{x}[]"
   puts "V: #{x}[]"
 end
