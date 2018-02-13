c3335d5f9af5522db414a7cc6c741684d5ea9c7b
Mars Santos <mars@meetup.com>
2018-02-13 02:56:11 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(x)
+def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   puts "R: [#{x}]"
   puts "O: [#{x}]"
   puts "Y: [#{x}]"
